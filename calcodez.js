96a5b079994ae7dd7e2736ccc477854da38c67d8
calvin <CalCodez@outlook.com>
2024-10-19 15:20:51 -0500

    js defaults functions setup

---
+++ calcodez.js
@@ -0,0 +1,11 @@
+//!!Default Functions
+
+const appendChild = (arg1, arg2) => arg1.appendChild(arg2);
+const getById = (arg1) => document.getElementById(arg1);
+const getByClass = (arg) => document.getElementsByClassName(arg);
+const addClassList = (arg1, arg2) => arg1.document.addClassList(arg2);
+const removeClassList = (arg1, arg2) => arg1.document.removeClassList(arg2);
+const toggleClassList = (arg1, arg2) => arg1.document.toggleClassList(arg2);
+const createElement = (arg1) => document.createElement(arg1);
+const selectAll = (arg) => document.querySelectorAll(arg);
+const select = (arg) => document.querySelector(arg);
