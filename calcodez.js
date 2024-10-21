a1c3820082e1033ab66fa052e14c0785c6d0db85
calvin <CalCodez@outlook.com>
2024-10-21 13:16:16 -0500

    working sidebar toggle elm display

--- calcodez.js
+++ calcodez.js
@@ -1,11 +1,34 @@
 //!!Default Functions
 
 const appendChild = (arg1, arg2) => arg1.appendChild(arg2);
-const getById = (arg1) => document.getElementById(arg1);
-const getByClass = (arg) => document.getElementsByClassName(arg);
-const addClassList = (arg1, arg2) => arg1.document.addClassList(arg2);
-const removeClassList = (arg1, arg2) => arg1.document.removeClassList(arg2);
-const toggleClassList = (arg1, arg2) => arg1.document.toggleClassList(arg2);
+const getId = (arg1) => document.getElementById(arg1);
+const getClass = (arg) => document.getElementsByClassName(arg);
+const addClass = (arg1, arg2) => arg1.classList.add(arg2);
+const removeClass = (arg1, arg2) => arg1.classList.remove(arg2);
+const toggleClass = (arg1, arg2) => arg1.classList.toggle(arg2);
 const createElement = (arg1) => document.createElement(arg1);
 const selectAll = (arg) => document.querySelectorAll(arg);
 const select = (arg) => document.querySelector(arg);
+
+const textDisplay = {
+	user: 'About',
+	project: 'Projects',
+	medium: 'Medium',
+	training: 'Training',
+};
+
+const menuToggler = getId('main-toggler');
+const mainMenu = getId('menu');
+const sideBarIcons = getClass('side-bar-icons');
+const activeIcons = getClass('active-icons');
+const click = 'click';
+const active = 'active-menu';
+const [userIcon, projectIcon, mediumIcon, trainingIcon] = sideBarIcons;
+
+menuToggler.addEventListener(click, function () {
+	if (!menuToggler.classList.contains(active)) {
+		toggleClass(mainMenu, active);
+	} else {
+		toggleClass(mainMenu, active);
+	}
+});
