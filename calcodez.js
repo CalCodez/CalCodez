11e2d4b584e9c2ee03361d2b1ec301ccc0369f53
calvin <CalCodez@outlook.com>
2024-10-28 15:19:41 -0500

    project section toggles and images added

--- calcodez.js
+++ calcodez.js
@@ -1,6 +1,7 @@
 //!!Default Functions
 
 const appendChild = (arg1, arg2) => arg1.appendChild(arg2);
+const removeChild = (arg1, arg2) => arg1.removeChild(arg2);
 const getId = (arg1) => document.getElementById(arg1);
 const getClass = (arg) => document.getElementsByClassName(arg);
 const addClass = (arg1, arg2) => arg1.classList.add(arg2);
@@ -9,6 +10,7 @@ const toggleClass = (arg1, arg2) => arg1.classList.toggle(arg2);
 const createElement = (arg1) => document.createElement(arg1);
 const selectAll = (arg) => document.querySelectorAll(arg);
 const select = (arg) => document.querySelector(arg);
+const textContent = (arg1, arg2) => (arg1.textContent = arg2);
 
 const menuToggler = getId('main-toggler');
 const mainMenu = getId('menu');
@@ -38,7 +40,8 @@ menuToggler.addEventListener('click', function () {
 });
 
 const sectionToggle = getClass('section-toggle');
-const [aboutToggle, projectToggle, trainingToggle] = sectionToggle;
+const [aboutSectionToggle, projectSectionToggle, trainingSectionToggle] =
+	sectionToggle;
 
 const sections = selectAll('section');
 
@@ -62,26 +65,114 @@ const toggleSection = (button, section) => {
 	});
 };
 
-toggleSection(aboutToggle, aboutSection);
-toggleSection(trainingToggle, trainingSection);
+toggleSection(aboutSectionToggle, aboutSection);
+toggleSection(trainingSectionToggle, trainingSection);
 
 const sectionLabels = getClass('section-label');
 const [aboutLabel, projectLabel, trainingLabel] = sectionLabels;
+const spanContainer = getId('spanContainer');
 
-const cardContainer = getId('cardContainer');
-
-projectToggle.addEventListener('click', function () {
+projectSectionToggle.addEventListener('click', function () {
 	if (!projectSection.classList.contains(sectionActive)) {
 		toggleClass(projectSection, sectionActive);
-		removeClass(projectToggle, toggleDown);
-		addClass(projectToggle, toggleUp);
-		toggleClass(cardContainer, 'card-container-active');
+		removeClass(projectSectionToggle, toggleDown);
+		addClass(projectSectionToggle, toggleUp);
+		toggleClass(spanContainer, 'span-container-active');
 		projectLabel.style.display = 'none';
 	} else {
 		toggleClass(projectSection, sectionActive);
-		toggleClass(cardContainer, 'card-container-active');
-		removeClass(projectToggle, toggleUp);
-		addClass(projectToggle, toggleDown);
+		toggleClass(spanContainer, 'span-container-active');
+		removeClass(projectSectionToggle, toggleUp);
+		addClass(projectSectionToggle, toggleDown);
 		projectLabel.style.display = 'block';
 	}
+});
+
+const cardToggles = getClass('card-toggles');
+const [
+	saasToggler,
+	pokemonToggler,
+	caltubeToggler,
+	responsiveToggler,
+	socialToggler,
+	favzToggler,
+	tributeToggler,
+	googleToggler,
+] = cardToggles;
+const cardWrapper = getId('cardWrapper');
+const projectCard = getId('projectCard');
+
+const source = {
+	saas: { src: './assets/project_card_images/SaaS.png', alt: 'SaaS' },
+	pokemon: {
+		src: './assets/project_card_images/Pokemon.png',
+		alt: 'Pokemon Card Collection',
+	},
+	calTube: { src: './assets/project_card_images/Caltube.png', alt: 'CalTube' },
+	responsive: {
+		src: './assets/project_card_images/Responsive_website.png',
+		alt: 'Responsive Website',
+	},
+
+	social: {
+		src: './assets/project_card_images/Social_Media.png',
+		alt: 'Social Media Logins',
+	},
+
+	favz: { src: './assets/project_card_images/My_Favz.png', alt: 'My Favz' },
+	tribute: {
+		src: './assets/project_card_images/Aaliyah_tribute.png',
+		alt: 'Aaliyah Tribute Page',
+	},
+	google: {
+		src: './assets/project_card_images/Google.png',
+		alt: 'Google Home Page',
+	},
+};
+
+const { saas, pokemon, calTube, responsive, social, favz, tribute, google } =
+	source;
+
+//++Project Toggle Variables */
+
+let imageContainer = getId('imageContainer');
+let projectTitleContainer = getId('projectTitleContainer');
+let img = createElement('img');
+
+//^^Project Toggle Function
+const toggleProjectCards = (toggler, imageSrc, imageAlt, projectTitle) => {
+	const click = 'click';
+	const active = 'flex-active';
+	toggler.addEventListener(click, function () {
+		if (!cardWrapper.classList.contains(active)) {
+			toggleClass(cardWrapper, active);
+			appendChild(imageContainer, img);
+			img.src = imageSrc;
+			img.alt = imageAlt;
+			textContent(projectTitleContainer, projectTitle);
+		}
+	});
+};
+
+toggleProjectCards(saasToggler, saas.src, saas.alt, saas.alt);
+toggleProjectCards(pokemonToggler, pokemon.src, pokemon.alt, pokemon.alt);
+toggleProjectCards(caltubeToggler, calTube.src, calTube.alt, calTube.alt);
+toggleProjectCards(
+	responsiveToggler,
+	responsive.src,
+	responsive.alt,
+	responsive.alt
+);
+toggleProjectCards(socialToggler, social.src, social.alt, social.alt);
+toggleProjectCards(favzToggler, favz.src, favz.alt, favz.alt);
+toggleProjectCards(tributeToggler, tribute.src, tribute.alt, tribute.alt);
+toggleProjectCards(googleToggler, google.src, google.alt, google.alt);
+
+//**Close Card Wrapper function */
+const wrapperExit = getId('wrapperExit');
+wrapperExit.addEventListener('click', function () {
+	toggleClass(cardWrapper, 'flex-active');
+	img.removeAttribute('src');
+	img.removeAttribute('alt');
+	console.log(img);
 });
